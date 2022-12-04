937432b8e0e175db8a059ec6a9d33315f63cc071
rezahashemi <reza.8139@gmail.com>
2022-12-04 21:29:52 +0330

    sync system

--- configs/sync.js
+++ configs/sync.js
@@ -1,11 +1,13 @@
-const sequelize = require("./db");
 const User = require("../models/user");
 const Account = require("../models/account");
 const Loan = require("../models/loan");
 const MembershipFee = require("../models/membershipFee");
 
 const syncTables = async () => {
-  await sequelize.sync({ alter: true });
+  await User.sync({ alter: true });
+  await Account.sync({ alter: true });
+  await Loan.sync({ alter: true });
+  await MembershipFee.sync({ alter: true });
 };
 
 module.exports = syncTables;
