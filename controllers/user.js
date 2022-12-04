84fd1a14514221c006fa2eb3cc7acdd7e6e11f54
rezahashemi <reza.8139@gmail.com>
2022-12-04 21:04:25 +0330

    change api

--- controllers/user.js
+++ controllers/user.js
@@ -3,20 +3,24 @@ const createUser = require("../services/user/create");
 const registerValidation = require("../schema/user/register");
 const checkUniqueValues = require("../services/user/checkUnique");
 const checkUser = require("../services/user/signin");
-const bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
 const env = process.env;
 
 const register = async (req, res) => {
   const receivedData = req.body;
   receivedData.password = bcrypt.hashSync(receivedData.password, 5);
   try {
-    // validate values 
-    const {error} = await registerValidation(receivedData);
-    if (error) {throw error}
-    // check being unique in db 
+    // validate values
+    const { error } = await registerValidation(receivedData);
+    if (error) {
+      throw error;
+    }
+    // check being unique in db
     const isExist = await checkUniqueValues(receivedData);
-    if (isExist) {throw isExist}
+    if (isExist) {
+      throw isExist;
+    }
     //create user
     await createUser(receivedData);
     res.status(201).json({ success: true });
@@ -29,30 +33,33 @@ const signin = async (req, res) => {
   const receivedData = req.body;
   try {
     // verify email and password
-    const { error , verifiedUser } = await checkUser(receivedData);
-    if (error) {throw error}
+    const { verifiedUser } = await checkUser(receivedData);
+    // if (error) {throw error}
     // create token
-    const token = jwt.sign({ verifiedUser }, env.SECRET_KEY, {expiresIn: "1 day"});
-    res.status(200).json({ sucess: true , token: token})
+    const token = jwt.sign({ verifiedUser }, env.SECRET_KEY, {
+      expiresIn: "1 day",
+    });
+    res.status(200).json({ sucess: true, token: token });
   } catch (error) {
-    res.status(401).json({ error })
+    res.status(400).json({ sucess: false, error });
   }
-  
 };
 
 const tokenCheck = async (req, res) => {
   try {
-    jwt.verify(req.header('token'), env.SECRET_KEY, (err, result) => {
-      if (err) {throw {error: "not authorized"}}
-      res.status(200).json({ result })
+    jwt.verify(req.header("token"), env.SECRET_KEY, (err, result) => {
+      if (err) {
+        throw { error: "not authorized" };
+      }
+      res.status(200).json({ result });
     });
   } catch (error) {
-    res.status(401).json({ error })
+    res.status(401).json({ error });
   }
 };
 
 router.post("/register", register);
 router.post("/signin", signin);
-router.get("/tokenCheck", tokenCheck)
+router.get("/tokenCheck", tokenCheck);
 
 module.exports = router;
--- services/user/signin.js
+++ services/user/signin.js
@@ -1,23 +1,26 @@
 const user = require("../../models/user");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const checkUser = async (data) => {
-    try {
-        const foundUser = await user.findOne({
-            where: {emailAddress: data.emailAddress}
-            }
-        );
-        const verification = bcrypt.compareSync(data.password, foundUser.dataValues.password)
-        if (verification) {
-            delete foundUser.dataValues.password;
-            return {verifiedUser: foundUser.dataValues}
-        } else {
-            return {error: "password is not correct"}
-        }
-         
-    } catch (error) {
-        return {error: "email address not found"}
+  const error = "email address or password is not correct";
+  try {
+    const foundUser = await user.findOne({
+      where: { emailAddress: data.emailAddress },
+    });
+    const verification = bcrypt.compareSync(
+      data.password,
+      foundUser.dataValues.password
+    );
+    if (verification) {
+      delete foundUser.dataValues.password;
+      return { verifiedUser: foundUser.dataValues };
+    } else {
+      throw error;
     }
+  } catch (err) {
+    console.error(err);
+    throw error;
+  }
 };
 
-  module.exports = checkUser;
+module.exports = checkUser;
